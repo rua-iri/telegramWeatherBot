78c79a561f9bc7d4dbc66019ee94aeee1db3f3c1
rua-iri <rory.mcguigan@protonmail.com>
2023-10-22 23:08:16 +0300

    update: implement function to handle location sent by user and extract longitude and latitude
    resolves #1

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 
+import json
 import logging
 import os
 import typing
 import dotenv
+from telegram import Update
+from telegram.ext import (Application, CommandHandler, ContextTypes,
+                          MessageHandler, filters)
 
 
 dotenv.load_dotenv()
@@ -14,8 +18,31 @@ logging.basicConfig(
 )
 
 
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_html("This is the help message")
+
+
+# TODO make api calls using the data from this 
+# function to handle the location sent by a user
+async def weatherByLocation(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"{update.message.location.longitude} \n {update.message.location.latitude}")
+
+
+# TODO just for testing, remove this eventually
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+
 def main() -> None:
-    pass
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("help", help))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.add_handler(MessageHandler(
+                    filters.LOCATION & ~filters.COMMAND,
+                    weatherByLocation))
+
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 if __name__ == "__main__":
