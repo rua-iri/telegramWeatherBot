fdda3e130cb7ebbeb69ef050d14d1c68689790b9
rua-iri <rory.mcguigan@protonmail.com>
2024-08-30 22:49:05 +0300

    update: code formatting

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ import os
 import typing
 import dotenv
 from telegram import Update
-from telegram.ext import (Application, CommandHandler, ContextTypes,
-                          MessageHandler, filters)
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters
+)
+
+import helpers
+import constants
 
 
 dotenv.load_dotenv()
@@ -22,10 +30,46 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_html("This is the help message")
 
 
-# TODO make api calls using the data from this 
+# TODO use api calls passing long and latt
+# into helpers functions to generate response
+
 # function to handle the location sent by a user
-async def weatherByLocation(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"{update.message.location.longitude} \n {update.message.location.latitude}")
+async def weatherByLocation(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+):
+    user = update.message.from_user
+    print(user)
+    print(f"Longitude: {update.message.location.longitude}")
+    print(f"Latitude: {update.message.location.latitude}")
+
+    userData = {"name": f"{user.first_name} {user.last_name}",
+                "id": user.id, "username": user.username,
+                "longitude": update.message.location.longitude,
+                "latitude": update.message.location.latitude}
+
+    with open("userdata.json", "a") as jsonFile:
+        jsonFile.write(json.dumps(userData))
+        jsonFile.write("\n")
+
+    weatherAPIData = helpers.getWeatherAPIReport(
+        update.message.location.latitude, update.message.location.longitude)
+    weatherAPIMessage = constants.weather_api_response.format(
+        location=weatherAPIData[0],
+        condition=weatherAPIData[1],
+        temperature=weatherAPIData[2],
+        feelsLike=weatherAPIData[3],
+        wind=weatherAPIData[4],
+        humidity=weatherAPIData[5],
+        precipitation=weatherAPIData[6],
+    )
+
+    await update.message.reply_text(weatherAPIMessage)
+    location_message = f"""Longitude: {update.message.location.longitude}
+    Latitude: {update.message.location.latitude}
+    """
+    await update.message.reply_text()
+    await update.message.reply_text(location_message)
 
 
 # TODO just for testing, remove this eventually
