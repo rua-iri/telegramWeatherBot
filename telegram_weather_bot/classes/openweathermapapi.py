a339f5fecb584dc00a5eb1ae63231a424b9c805a
rua-iri <rory.mcguigan@protonmail.com>
2024-09-11 15:46:08 +0300

    update: open weather api class

--- telegram_weather_bot/classes/openweathermapapi.py
+++ telegram_weather_bot/classes/openweathermapapi.py
@@ -8,9 +8,30 @@ class OpenWeatherMapAPI(Base_API):
 
     def __init__(self, latitude: float, longitude: float):
         data = self.fetch_api_data(latitude=latitude, longitude=longitude)
+        weather_data = data.get('weather')[0]
+        main_data = data.get('main')
 
-        self.location = f"{data.get('name')}"
-        self.location += f", {data.get('sys').get('country')}"
+        self.location = f"{data.get('name')}, "
+        self.location += data.get('sys').get('country')
+
+        self.wind = f"{data.get('wind').get('speed')} m/s "
+        self.wind += self.calc_direction(data.get('wind').get('deg'))
+
+        self.humidity = main_data.get('humidity')
+
+        self.condition_text = f"{weather_data.get('main')} - {weather_data.get('description')}"
+        self.condition_icon = f"https://openweathermap.org/img/wn/{weather_data.get('icon')}@2x.png"
+
+        self.temperature = main_data.get('temp')
+        self.feels_like = main_data.get('feels_like')
+
+    def calc_direction(angle: int):
+        import math
+        angle_list = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE",
+                      "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+
+        index = math.floor((angle / 22.5) + 0.5)
+        return angle_list[index]
 
     def fetch_api_data(self, latitude: float, longitude: float):
         try:
@@ -26,7 +47,24 @@ class OpenWeatherMapAPI(Base_API):
         except Exception as e:
             raise e
 
+    def gen_weather_message(self) -> str:
+
+        return """
+        Open Weather
+        Location: {location}
+        Condition: {condition_text}
+        Temperature: {temperature}°C (Feels Like: {feels_like}°C)
+        Wind: {wind}
+        Humidity: {humidity}
+        """.format(**self.__dict__)
+
     def __repr__(self) -> str:
-        return (
-            f"Location: {self.location} \n"
-        )
+        return f"""
+        Location: {self.location},
+        Wind: {self.wind},
+        Humidity: {self.humidity}
+        Condition: {self.condition_text},
+        Icon: {self.condition_icon},
+        Temperature: {self.temperature},
+        Feels Like: {self.feels_like},
+        """
